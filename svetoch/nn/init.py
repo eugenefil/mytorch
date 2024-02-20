1da35208b16f806025db6fa91d0842f1af0e0190
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-02-20 19:30:29 +0200

    nn: init: Fix kaiming_normal_()

--- svetoch/nn/init.py
+++ svetoch/nn/init.py
@@ -1,5 +1,8 @@
-import svetoch
+import svetoch.tensor as ten
 
+# Strictly speaking, this is not an in-place operation, but, unlike
+# pytorch, there's no in-place normal() in numpy/cupy. Since init is a
+# one-time thing, this will hopefully not be a big deal.
 def kaiming_normal_(t):
     assert t.ndim >= 2
     if t.ndim == 2:
@@ -10,5 +13,5 @@ def kaiming_normal_(t):
             fan_in *= n
 
     std = (2. / fan_in)**.5
-    t.v = svetoch.normal(0., std, t.shape, dtype=t.dtype, device=t.device).v
+    t.array = ten.normal(0., std, t.shape, dtype=t.dtype, device=t.device).array
     return t
