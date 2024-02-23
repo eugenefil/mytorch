184f89912b1638b6a1ff817456b277d609ccdefb
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-02-23 19:25:17 +0200

    nn: functional: Fix and describe nll_loss

--- svetoch/nn/functional.py
+++ svetoch/nn/functional.py
@@ -22,7 +22,16 @@ def maxpool2d(x, ksize, stride=1, padding=0):
 
 def nll_loss(x, target):
     n = len(x)
-    return -x[x.am.arange(n), target.v].sum() / x.v.dtype.type(n)
+    # x is log-softmax output. Softmax outputs a probability distribution
+    # of (0, 1). Thus log-softmax is log[(0, 1)] = (-inf, 0). Negated
+    # log-softmax is (0, +inf). The correct class must have a probability
+    # close to 1 at softmax output and thus a positive close to 0 value
+    # at negated log-softmax output. This positive value, negated output
+    # of log-softmax for a correct class, is an error to be minimized.
+    # Final loss value is the sum of these errors for all input samples
+    # divided by the num of samples, i.e. averaged. Averaging is used
+    # so that final value does not depend on the number of samples.
+    return -x[x.backend.arange(n), target.array].sum() / x.array.dtype.type(n)
 
 
 def cross_entropy(x, target):
