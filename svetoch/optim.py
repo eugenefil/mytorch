a49a5323e8b4ea1161ee9ac6b7dd6a13a16fa505
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-02-23 19:24:32 +0200

    optim: Fix and describe sgd

--- svetoch/optim.py
+++ svetoch/optim.py
@@ -1,12 +1,19 @@
+import svetoch.autograd as ag
 
 class SGD:
+    # Unlike torch, zero gradients in the optimizer by default, b/c it's
+    # easy to forget to do this, so the gradients will accumulate.
+    # As a little optimization, instead of zeroing set gradients to None
+    # with set_to_none=True.
+    # Unlike weight_decay in torch, l2_decay is not multiplied by
+    # (i.e. it's decoupled from) learning rate.
     def __init__(self, params, lr, l2_decay=0., zero_grad=True,
                  set_to_none=False):
         self.params, self.lr, self.l2_decay = params, lr, l2_decay
         self._zero_grad = zero_grad
         self.set_to_none = set_to_none
 
-    @no_grad()
+    @ag.no_grad()
     def step(self):
         for p in self.params:
             if self.l2_decay > 0.:
